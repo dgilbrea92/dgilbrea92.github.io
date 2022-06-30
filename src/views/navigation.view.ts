93f36bc6128b9f217b97e5d84decfaaf35a2b3e5
Dakota <dakota.x.gilbreath.-nd@disney.com>
2022-06-30 15:58:06 -0700

    added better typing to nav views

--- src/views/navigation.view.ts
+++ src/views/navigation.view.ts
@@ -1,35 +1,42 @@
-import { makeHomeView } from "./home.view"
-import { makePortfolioView } from "./portfolio.view"
-import { renderView } from "./view.utils"
+import { makeHomeView } from './home.view';
+import { makePortfolioView } from './portfolio.view';
+import { renderView } from './view.utils';
+import { ViewFn } from './views.types';
 
-type NavItem = [label: string, viewFn: Function]
+type NavItem = [label: string, viewFn: ViewFn];
 
-const NAV_ITEMS: NavItem[] = [["Home", makeHomeView], ["Portfolio", makePortfolioView]]
+const NAV_ITEMS: NavItem[] = [
+    ['Home', makeHomeView],
+    ['Portfolio', makePortfolioView],
+];
 
-export function makeNavView() {
-    const wrapper = document.createElement("div")
+export function makeNavView(): HTMLDivElement {
+    const wrapper = document.createElement('div');
 
     NAV_ITEMS.forEach((navItem) => {
-        const navLink = makeNavLink(...navItem)
-        wrapper.appendChild(navLink)
-    })
-    
-    return wrapper
+        const navLink = makeNavLink(...navItem);
+        wrapper.appendChild(navLink);
+    });
+
+    return wrapper;
 }
 
-function makeNavLink(label: string, viewFn: Function) {
-    const link = document.createElement("a")
-  
-    link.textContent = label
-    link.addEventListener("click", handleNavClick(viewFn))
-    link.setAttribute("style", "padding: 2px; text-decoration: underline; cursor: pointer")
-  
-    return link
+function makeNavLink(label: string, viewFn: ViewFn): HTMLAnchorElement {
+    const link = document.createElement('a');
+
+    link.textContent = label;
+    link.addEventListener('click', handleNavClick(viewFn));
+    link.setAttribute(
+        'style',
+        'padding: 2px; text-decoration: underline; cursor: pointer'
+    );
+
+    return link;
 }
 
-function handleNavClick(viewFn: Function) {
+function handleNavClick(viewFn: ViewFn): (event: MouseEvent) => void {
     return function (event: MouseEvent) {
-      event.preventDefault()
-      renderView(viewFn())
-    }
-  }
+        event.preventDefault();
+        renderView(viewFn());
+    };
+}

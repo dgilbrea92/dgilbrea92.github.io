c0da4b538796648a192cc219608cab44818146d0
Dakota <dakota.x.gilbreath.-nd@disney.com>
2022-06-29 20:24:28 -0700

    added skeleton

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,10 @@
 import { makeHomeView } from "./views/home.view.js";
 import { renderView } from "./views/view.utils.js";
+import { makeNavView } from "./views/navigation.view.js";
 function init() {
     const navSection = document.getElementById("section-nav");
+    const navView = makeNavView();
+    navSection === null || navSection === void 0 ? void 0 : navSection.appendChild(navView);
     const homeView = makeHomeView();
     renderView(homeView);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { makeHomeView } from "./views/home.view.js"
 import { renderView } from "./views/view.utils.js"
+import { makeNavView } from "./views/navigation.view.js"
 
 function init() {
     const navSection = document.getElementById("section-nav")
+    const navView = makeNavView()
+    navSection?.appendChild(navView)
 
     const homeView = makeHomeView()
     renderView(homeView)
